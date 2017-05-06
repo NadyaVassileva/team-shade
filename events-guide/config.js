6e3122f19948e2921186a668cd3c0fec12a8817b
tinmanjk <tinmanjk@mail.com>
2017-05-06 21:00:39 +0300

    comments in config.js

--- events-guide/config.js
+++ events-guide/config.js
@@ -2,12 +2,16 @@ SystemJS.config({
     'map': {
         'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+        
+        //Libraries
         'jquery': './node_modules/jquery/dist/jquery.js',
         'sammy': './node_modules/sammy/lib/sammy.js',
         'handlebars': './node_modules/handlebars/handlebars.js',
 
+        //Utils
         'templateLoader': './scripts/templates.js',
 
+        //Main
         'sammyApp': '/scripts/sammyApp.js',
     },
         'transpiler': 'plugin-babel',

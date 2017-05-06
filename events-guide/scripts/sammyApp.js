b6b77ee30edd2d130fd8739f556bac39b1c611b5
tinmanjk <tinmanjk@mail.com>
2017-05-06 23:08:50 +0300

    fixed bug - first argument of Sammy constructor

--- events-guide/scripts/sammyApp.js
+++ events-guide/scripts/sammyApp.js
@@ -1,52 +1,48 @@
+import 'jquery';
 import Sammy from 'sammy';
-import 'jquery'; //importing everything from jquery with side effects
+
 import templates from 'templateLoader';
 
-window.addEventListener("hashchange", () => {
-  // console.log(location.hash);
-});
+  var sammyApp = Sammy('#container', function () {
+    let $container = $('#container');
+    this.get('#/', function() {
+        this.redirect('#/home');
+    });
 
+    //FIX ME
+    //seems that redirect does not triger
+    //next get
 
-var sammyApp = Sammy('container', function () {
-  let $container = $('#container');
-  // this.get('#/', function() {
-  //     this.redirect('#/home');
-  // });
+    //FIX ME
+    //Links on the tabs do not trigger as well
 
-  //FIX ME
-  //seems that redirect does not triger
-  //next get
+    //need to refactor to controllers
+    this.get('#/home', function () {
+      templates.load('home')
+        .then(function (templateHtml) {
+          $container.html(templateHtml);
+        });
+    });
 
-  //FIX ME
-  //Links on the tabs do not trigger as well
+    this.get('#/cinema', () => {
+      console.log("CINEMA");
+    });
 
-  //need to refactor to controllers
-  this.get('#/home', function () {
-    templates.load('home')
-      .then(function (templateHtml) {
-        $container.html(templateHtml);
-      });
+    this.route('#/login', function () {
+      console.log("login");
+      templates.load('login')
+        .then(function (templateHtml) {
+          $container.html(templateHtml);
+        });
+    });
+
+    this.get('#/login', function () {
+      templates.load('login')
+        .then(function (templateHtml) {
+          $container.html(templateHtml);
+        });
+    });
+
   });
 
-  this.get('#/cinema', () => {
-    console.log("CINEMA");
-  });
-
-  this.route('#/login', function () {
-    console.log("login");
-    templates.load('login')
-      .then(function (templateHtml) {
-        $container.html(templateHtml);
-      });
-  });
-
-  this.get('#/login', function () {
-    templates.load('login')
-      .then(function (templateHtml) {
-        $container.html(templateHtml);
-      });
-  });
-
-});
-
-sammyApp.run('#/');
+  sammyApp.run('#/');

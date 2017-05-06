a34ba893ebf7a25ff1f4e94ae5f06753cdac2a41
tinmanjk <tinmanjk@mail.com>
2017-05-06 21:11:23 +0300

    bug analysis

--- events-guide/scripts/sammyApp.js
+++ events-guide/scripts/sammyApp.js
@@ -1,29 +1,37 @@
 import Sammy from 'sammy';
-import $ from 'jquery';
+import 'jquery'; //importing everything from jquery with side effects
 import templates from 'templateLoader';
 
 var sammyApp = Sammy('container', function() {
   let $container = $('#container');
   this.get('#/', function() {
-      this.redirect('#/home')
+      this.redirect('#/home');
   });
+
+  //FIX ME
+  //seems that redirect does not triger
+  //next get
+
+  //FIX ME
+  //Links on the tabs do not trigger as well
+
   this.get('#/home', function() {
     templates.load('home')
     .then(function(templateHtml) {
       $container.html(templateHtml);
-    })
+    });
   });
   this.get('#/login', function() {
     templates.load('login')
     .then(function(templateHtml) {
       $container.html(templateHtml);
-    })
+    });
   });
     this.get('#/login', function() {
     templates.load('login')
     .then(function(templateHtml) {
       $container.html(templateHtml);
-    })
+    });
   });
 });
 

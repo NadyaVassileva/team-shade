b0b5d8549082e1f22c4a5b6750d746a59d9f5a65
NadyaVassileva <nadya_vassileva@yahoo.com>
2017-05-08 12:50:42 +0300

    favorites name corrected

--- events-guide/js/models/event.js
+++ events-guide/js/models/event.js
@@ -1,11 +1,11 @@
 class Event {
-    constructor(category, name, image, description, location, date, favorities){
+    constructor(category, name, image, description, location, date, favorites){
         this.category = category;
         this.name = name;
         this.image = image;
         this.description = description;
         this.location =location;
         this.date = date;
-        this.favorities = [];
+        this.favorites = [];
     }
 }
